8c96922fc5b30954eee5083c0f7855a16a9da669
Sami Noroozi <68302126+potatohunter69@users.noreply.github.com>
2024-01-24 01:30:38 +0100

    fixed routing bug

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -65,7 +65,7 @@ function NavBar() {
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="ms-auto" defaultActiveKey="#home">
             <Nav.Item>
-              <Nav.Link as={Link} to="/" onClick={() => updateExpanded(false)}>
+              <Nav.Link as={Link} to="/My_Web" onClick={() => updateExpanded(false)}>
                 <AiOutlineHome style={{ marginBottom: "2px" }} /> Home
               </Nav.Link>
             </Nav.Item>
