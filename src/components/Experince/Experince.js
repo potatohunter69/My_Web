0e149f5b57916de146037f37aea28507145b3f30
Sami Noroozi <68302126+potatohunter69@users.noreply.github.com>
2024-01-29 18:31:46 +0100

    fixed the styling error for experince component

--- src/components/Experince/Experince.js
+++ src/components/Experince/Experince.js
@@ -5,27 +5,12 @@ import "../../style.css"; // Ensure this CSS is being imported
 import AOS from "aos";
 import "aos/dist/aos.css";
 import { useEffect } from "react";
+import Particle from "../Particle";
 
 function Experience() {
   useEffect(() => {
     AOS.init();
   }, []);
-  // Inline styles
-  const sectionStyle = {
-    paddingTop: "110px",
-    paddingBottom: "30px",
-    color: "white",
-    position: "relative",
-    backgroundImage: "var(--image-gradient), url(./home-bg.jpg)", // Using CSS variable and background image
-    backgroundPosition: "top center",
-    backgroundRepeat: "no-repeat",
-  };
-
-  const containerStyle = {
-    maxWidth: "1200px",
-    margin: "0 auto",
-    padding: "20px",
-  };
 
   const titleStyle = {
     fontSize: "3em",
@@ -36,14 +21,11 @@ function Experience() {
   };
 
   return (
-    <section
-      className="section home-section"
-      id="experience"
-      style={sectionStyle}
-    >
+    <section className="resume-section">
+      <Particle/>
       {" "}
       {/* Added 'home-section' class */}
-      <div className="container" style={containerStyle}>
+      <div className="container">
         <h1 className="title purple" style={titleStyle}>
           We are here{" "}
         </h1>{" "}
